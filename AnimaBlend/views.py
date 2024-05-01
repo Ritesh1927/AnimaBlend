b420ac5677482b37e45a4f308ebc1a47b05041fb
Aditya Singh <adityasinghedneed@gmail.com>
2024-05-01 20:43:54 +0530

    episode's code

--- AnimaBlend/views.py
+++ AnimaBlend/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from .models import Episode , Anime
 
 # Create your views here.
 
@@ -9,7 +10,9 @@ def index(request):
     return render(request, 'AnimaBlend/index.html')
 
 def play(request):
-    return render(request,'AnimaBlend/play.html')
+    anime = Anime.object.get(title='Vinland Saga Season 1')
+    episode = Episode.object.filter(anime=anime)
+    return render(request,'AnimaBlend/play.html',{'episodes':episode})
 
 def details(request):
     return render(request,'AnimaBlend/details.html')
