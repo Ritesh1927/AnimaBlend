54ae8862b5681a027c40a299551aa2eec00dbcf7
Ritesh <riteshyadav1927@gmail.com>
2024-09-19 18:14:39 +0530

    made fixes on details page

--- AnimaBlend/urls.py
+++ AnimaBlend/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 from AnimaBlend import views
 
 urlpatterns = [
-    path('', views.index, name=''),
+    path('', views.index, name='index'),
     path('home',views.home,name='home'),
-    path('play', views.play, name='play'),
     path('anime/<int:anime_id>/', views.details, name='details'),
     path('search', views.search, name='search'),
+    path('anime/<int:anime_id>/play/', views.play, name='play_one'),
 ]
--- AnimaBlend/views.py
+++ AnimaBlend/views.py
@@ -10,8 +10,11 @@ def home(request):
 def index(request):
     return render(request, 'AnimaBlend/index.html')
 
-def play(request):
-    return render(request,'AnimaBlend/play.html')
+def play(request, anime_id, episode_num=1):
+    anime = get_object_or_404(Anime, pk=anime_id)
+    episode = get_object_or_404(Episode, anime=anime, episode_number=episode_num)
+    episodes = Episode.objects.filter(anime=anime).order_by('episode_number')
+    return render(request,'AnimaBlend/play.html', {'anime': anime, 'current_episode': episode, 'episodes': episodes})
 
 def details(request, anime_id):
     anime = get_object_or_404(Anime, pk=anime_id)
