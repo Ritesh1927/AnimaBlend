8e3a323eb875e4b271b77818aa33c50c345738f1
Ritesh <riteshyadav1927@gmail.com>
2025-05-30 20:08:44 +0530

    minor fixes

--- Dictators/settings.py
+++ Dictators/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from pathlib import Path, os
+from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +28,7 @@ SECRET_KEY = 'django-insecure-!ttbpr#d($ze&2zd96lkmbyzaurku0f1_bvbqte6q27sdt1738
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.29.172']
+ALLOWED_HOSTS = []
 
 
 # Application definition
